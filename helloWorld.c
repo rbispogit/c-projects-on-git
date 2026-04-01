64f59a184456673041b5d78fdce57a589d053bd5
Robson <rbispo.eng@gmail.com>
2026-03-31 21:08:45 -0300

    Exercitando ponteiros

--- helloWorld.c
+++ helloWorld.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
+
+    int numero = 10;
+    int* p = &numero;
+
+    printf("Valor da variável 'numero': %d\n", numero);
+    printf("Endereço da variável 'numero': %p\n", &numero);
+    printf("Valor apontado pelo ponteiro 'p': %d\n", *p);
+    printf("Conteúdo do Ponteiro: %p\n", p);
+    
+    *p = 20;
+
+    printf("Novo valor de 'numero', usando o ponteiro: %d\n", numero);
 }
