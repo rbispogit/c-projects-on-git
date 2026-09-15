b5b663be693d9eb9007598b3d34635deab5d7c4e
Robson <rbispo.eng@gmail.com>
2026-09-15 00:15:55 -0300

    Alterações em ponteiros

--- helloWorld.c
+++ helloWorld.c
@@ -15,21 +15,23 @@ int main()
 
     // Exibindo os endereços de variaveis.
     printf("Endereço da váriavel 'numero': %x\n", &numero);
+    printf("Endereço da variável 'nome': %x\n", &nome);
     printf("Endereço da variável(ponteiro) '*p': %x\n", &p);
     printf("Endereço da variável(ponteiro) '**ptrp': %x\n", &ptrp);
-    printf("Endereço do ponteiro 'ptrN': %x\n", &ptrN);
+    printf("Endereço da variável(ponteiro) *ptrN: %x\n", &ptrN);
 
     // Exibindo os conteudos das variaveis.
     printf("\nConteúdo da variável 'numero': %d\n", numero);
     printf("Conteúdo da variável '*p': %x\n", p);
     printf("Conteúdo da variável '**ptrp': %x\n", ptrp);
     printf("Conteúdo da variável '*ptrN': %x\n", ptrN);
+    printf("Conteúdo da variável 'nome': %s\n", nome);
 
-    // Alterando valor da variavel 'numero', através do segundo ponteiro.
+    // Alterando valor das variáveis através do segundo ponteiro.
     **ptrp = **ptrp + 20;
 
     // Exibindo novo valor da variável 'numero'.
     printf("\nNovo valor da variável 'numero': %d\n", numero);
-
+    printf("Novo valor da variável 'nome': %s\n", nome);
     return 0;
 }
